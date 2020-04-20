88b17ccfdbf68fac3fe60ea4d995544bfffe7941
nsbernstein52 <nsbernstein52@gmail.com>
2020-04-20 13:38:43 -0600

    Add some INSERTs and SELECTs

---
+++ db/schema.js
@@ -0,0 +1,132 @@
+DROP DATABASE IF EXISTS resources_db;
+
+CREATE DATABASE resources_db;
+
+c\ resources_db;
+
+CREATE TABLE resources (
+  resource_id SERIAL PRIMARY KEY,
+  description TEXT,
+  level TEXT,
+  link TEXT
+);
+
+
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+CREATE TABLE resources_contributors (
+  resource_contributor_id SERIAL PRIMARY KEY,
+  resource_id INT REFERENCES topics(topics_id),
+  contributor_id INT REFERENCES users(users_id)
+);
+
+
+CREATE TABLE topics (
+  topic_id SERIAL PRIMARY KEY,
+  topic TEXT,
+  abbrev TEXT,
+);
+
+CREATE TABLE resources_topics (
+  resource_topic_id SERIAL PRIMARY KEY,
+  resource_id INT REFERENCES topics(topics_id),
+  topic_id INT REFERENCES topics(topics_id)
+);
+
+// INSERT seeding
+INSERT INTO resources VALUES ("a test description", "All");
+
+INSERT INTO users VALUES ("Neil", "Snail", "thesnail@tbd.com");
+INSERT INTO resources_users VALUES (1, 1);
+
+INSERT INTO topics VALUES ("aaaTestTopic", "aTT");
+INSERT INTO resources_topics VALUES (1, 1);
+
+// SELECT QUERY seeding
+SELECT * FROM resources;
+SELECT * FROM users;
+SELECT * from topics;
+
+// SELECT topics.topic, topics.abbrev, resources.level, resources.link, resources.description, users.first_name, users.lastname, users.email
+// FROM resources
+// INNER JOIN topics ON resources.resource_id = ...
+
+//////////
+
+// CREATE TABLE ys (
+//   y_id SERIAL PRIMARY KEY,
+//   y TEXT
+// );
+
+// CREATE TABLE xs_ys (
+//   x_y_id SERIAL PRIMARY KEY,
+//   x_id INT REFERENCES xs(x_id),
+//   y_id INT REFERENCES ys(y_id)
+// );
+
+
+// CREATE TABLE apps (
+//   app_id SERIAL PRIMARY KEY,
+//   app TEXT
+// );
+
+// CREATE TABLE resources_apps (
+//   resource_app_id SERIAL PRIMARY KEY,
+//   resource_id INT REFERENCES resources(resource_id),
+//   app_id INT REFERENCES apps(app_id)
+// );
+
+
+// CREATE TABLE relatedresources (
+//   relatedresource_id SERIAL PRIMARY KEY,
+//   relatedresource TEXT
+// );
+
+// CREATE TABLE resources_relatedresources (
+//   resource_relatedresource_id SERIAL PRIMARY KEY,
+//   resource_id INT REFERENCES resources(resource_id),
+//   relatedresource_id INT REFERENCES relatedresources(relatedresource_id)
+// );
+
+
+
+// CREATE TABLE sprints (
+//   sprint_id SERIAL PRIMARY KEY,
+//   sprint TEXT
+// );
+
+// CREATE TABLE resources_sprints (
+//   resource_sprint_id SERIAL PRIMARY KEY,
+//   resource_id INT REFERENCES resources(resource_id),
+//   sprint_id INT REFERENCES sprints(sprint_id)
+// );
+
+
+// CREATE TABLE subresources (
+//   subresource_id SERIAL PRIMARY KEY,
+//   subresource TEXT
+// );
+
+// CREATE TABLE resources_subresources (
+//   resource_subresource_id SERIAL PRIMARY KEY,
+//   resource_id INT REFERENCES resources(resource_id),
+//   subresource_id INT REFERENCES rsubresources(subresource_id)
+// );
+
+
+// CREATE TABLE tags (
+//   tag_id SERIAL PRIMARY KEY,
+//   tag TEXT
+// );
+
+// CREATE TABLE resources_tags (
+//   resource_tag_id SERIAL PRIMARY KEY,
+//   resource_id INT REFERENCES resources(resources_id),
+//   tag_id INT REFERENCES tags(tags_id)
+// );
+
