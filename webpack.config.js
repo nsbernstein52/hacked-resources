544a8b9775c3c530468fdc4b85000c407552c5a9
nsbernstein52 <nsbernstein52@gmail.com>
2020-04-21 12:48:50 -0600

    Debug to get webpack working

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+console.log("wc: ", path.resolve(__dirname, 'dist'))
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?/s,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        loaders: ['style-loader', 'css-loader'],
+      }
+    ],
+  },
+  node: {
+    fs: 'empty'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
+
+
+
+
+
