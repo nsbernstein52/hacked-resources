d0d48e9c046fd6980d326ca8a934e0405deeebed
nsbernstein52 <nsbernstein52@gmail.com>
2020-05-11 14:03:55 -0600

    Continue filling out AddResourceHandler

--- src/comp/AddResourceHandler.jsx
+++ src/comp/AddResourceHandler.jsx
@@ -2,20 +2,21 @@
 
 import React from 'react';
 import Row from 'react-bootstrap/Row';
+// import Row from 'react-bootstrap/Col';
 
 class AddResourceHandler extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-          abbrev: null,
-          contributor: null,
-          description: null,
-          level: null,
-          link: null,
-          topic: null
-        };
-    }
+    this.state = {
+      abbrev: null,
+      contributor: null,
+      description: null,
+      level: null,
+      link: null,
+      topic: null
+    };
+  }
 
     handleChange = (e) => {
         this.setState({ [e.target.name]: e.target.value });
@@ -23,34 +24,34 @@ class AddResourceHandler extends React.Component {
 
     render() {
       return (
-          <form id="addResourceForm">
-              <Row>
-              &nbsp;&nbsp; Topic: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <input name="topic" onChange={this.handleChange} placeholder="topic ..." type="text" size="50" />  
-              </Row>
-              <Row>
-              &nbsp;&nbsp; Link: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;    <input name="link" onChange={this.handleChange} placeholder="link ..." type="text" size="70" /> 
-              </Row>
-              <Row>
-              &nbsp;&nbsp; Abbreviation: &nbsp;  <input name="abbrev" onChange={this.handleChange} placeholder="abbrev..." type="text" size="10" />                  
-              </Row>
-              <Row>
-              &nbsp;&nbsp; Level: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  <input name="level" onChange={this.handleChange} placeholder="level ..." type="text" size="5" /> 
-              </Row>
-              <Row>                 
-              &nbsp;&nbsp; Contributor: &nbsp; &nbsp;  <input name="contributor" onChange={this.handleChange} placeholder="contributor ..." type="text" size="20" /> 
-              </Row>
-              <Row>
-              &nbsp;&nbsp; Description: &nbsp; &nbsp;  <input name="description" onChange={this.handleChange} placeholder="description ..." size="70" /> 
-              </Row>
-               <button text-color="red" onClick={(e) => this.props.addResource(e, 
-                  this.state.abbrev, 
-                  this.state.contributor, 
-                  this.state.description,
-                  this.state.level, 
-                  this.state.link, 
-                  this.state.topic
-                )}><b>Add A Resource</b></button>
-            </form>
+        <form id="addResourceForm">
+            <Row>
+                &nbsp;&nbsp; <b>Topic:</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <input name="topic" onChange={this.handleChange} placeholder="topic ..." type="text" size="50" />  
+            </Row>
+            <Row>
+            &nbsp;&nbsp; <b>Link:</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;    <input name="link" onChange={this.handleChange} placeholder="link ..." type="text" size="70" /> 
+            </Row>
+            <Row>
+            &nbsp;&nbsp; <b>Abbreviation:</b> &nbsp;  <input name="abbrev" onChange={this.handleChange} placeholder="abbrev..." type="text" size="10" />                  
+            </Row>
+            <Row>
+            &nbsp;&nbsp; <b>Level:</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  <input name="level" onChange={this.handleChange} placeholder="level ..." type="text" size="5" /> 
+            </Row>
+            <Row>                 
+            &nbsp;&nbsp; <b>Contributor:</b> &nbsp; &nbsp;  <input name="contributor" onChange={this.handleChange} placeholder="contributor ..." type="text" size="20" /> 
+            </Row>
+            <Row>
+            &nbsp;&nbsp; <b>Description:</b> &nbsp; &nbsp;  <input name="description" onChange={this.handleChange} placeholder="description ..." size="70" /> 
+            </Row>
+            <button text-color="red" onClick={(e) => this.props.addResource(e, 
+                this.state.abbrev, 
+                this.state.contributor, 
+                this.state.description,
+                this.state.level, 
+                this.state.link, 
+                this.state.topic
+            )}><b>Add A Resource</b></button>
+        </form>
         )
     }
 }
