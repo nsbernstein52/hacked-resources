923f64225b1f5d87dc84084c8d95ad529eb233e5
nsbernstein52 <nsbernstein52@gmail.com>
2020-05-15 17:38:45 -0600

    Update Pool argument for Heroku

--- db/query.js
+++ db/query.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg')
-const pool = new Pool({
+const pool = new Pool(process.env.DATABASE_URL || {
+  
   database: 'resources_db',
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
