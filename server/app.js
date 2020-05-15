a8136e16ec5ae54a2f6017a893242452b9a2f6c4
nsbernstein52 <nsbernstein52@gmail.com>
2020-05-15 16:31:27 -0600

    Create routes for get / and get dist/bundle.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 26673; // Heroku default port
 
 // const bodyParser = require('body-parser');
 
@@ -26,7 +27,16 @@ app.use(express.json());
 let entryTime = new Date();
 // console.log(new Date());
 console.log("a.js: ENTERING", new Date());
+console.log("PORT: ", PORT);
+console.log(__dirname);
 
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/../public/index.html'))
+});
+
+app.get('/dist/bundle.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/../dist/bundle.js'))
+});
 
 ////// CRUD
 
