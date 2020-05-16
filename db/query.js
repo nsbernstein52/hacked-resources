4ee6366df9fffeca26a7dbc249760dc5ef3f682c
nsbernstein52 <nsbernstein52@gmail.com>
2020-05-16 16:38:41 -0600

    Lint

--- db/query.js
+++ db/query.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+
 let pool;
 if (!process.env.DATABASE_URL) {
   pool = new Pool({
@@ -13,114 +14,116 @@ if (!process.env.DATABASE_URL) {
   });
 }
 
-// console.log("q: process.env.DB_HOST: ", process.env.DB_HOST);
-// console.log("q: process.env.DB_USER: ", process.env.DB_USER);
-// console.log("q: process.env.DB_USER: ", process.env.DB_PASS);
+// console.log('q: process.env.DB_HOST: ', process.env.DB_HOST);
+// console.log('q: process.env.DB_USER: ', process.env.DB_USER);
+// console.log('q: process.env.DB_USER: ', process.env.DB_PASS);
 
 // //// TEST POOL:  the pool will emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
 pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err)
-  process.exit(-1)
-})
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
 
 // //// TEST CLIENT:  callback - checkout a client
 pool.connect((err, client, done) => {
-  if (err) throw err
+  if (err) throw err;
   client.query('SELECT * FROM resources WHERE id = $1', [1], (err, res) => {
-    done()
+    done();
     if (err) {
-      console.log("q: p.c: err: ", err.stack)
+      console.log('q: p.c: err: ', err.stack);
     } else {
-      // console.log("q: p.c res.rows[0]: ", res.rows[0])
+      // console.log('q: p.c res.rows[0]: ', res.rows[0])
     }
-  })
-})
+  });
+});
 
 // //// CRUD
 
 // //// addResource
 const addResource = (resource) => {
-  let values = [resource.abbrev, resource.contributor, resource.description, resource.level, resource.link, resource.topic];
-  // console.log("q: aR: ENTERED");
+  const values = [resource.abbrev, resource.contributor, resource.description, resource.level,
+    resource.link, resource.topic];
+  // console.log('q: aR: ENTERED');
   return pool.query('INSERT INTO resources (abbrev, contributor, description, level, link, topic) VALUES ($1, $2, $3, $4, $5, $6)', values)
-  .then(res => {
-    // console.log("q: gARs r.r[3]:", res.rows[3]);
-    // console.log("q: aR: COMPLETED");
+    .then((res) => {
+    // console.log('q: gARs r.r[3]:', res.rows[3]);
+    // console.log('q: aR: COMPLETED');
     return res.rows;
-  })
-  .catch(err => {console.error("error from DB", err)})
+    })
+    .catch((err) => {console.error('error from DB', err)});
 };
 
 // //// deleteResource
 const deleteResource = (id) => {
-  let values = [id]
+  const values = [id];
   return pool.query('DELETE FROM resources WHERE id = $1', values)
     .then(() => {
-      // console.log("q: dR: COMPLETED: ");
-      return true
-   })
-   .catch(err => {console.error("error from DB", err)})
+      // console.log('q: dR: COMPLETED: ');
+      return true;
+    })
+    .catch((err) => {console.error('error from DB', err)});
 };
 
 // //// getAllResources
 const getAllResources = () => {
-  // console.log("q: gARs: ENTERED");
-  return pool.query("SELECT * FROM resources")
-  .then(res => {
-    // console.log("q: gARs r.r[3]:", res.rows[3]);
-    return res.rows;
-  })
-  .catch(err => {console.error("error from DB", err)})
+  // console.log('q: gARs: ENTERED');
+  return pool.query('SELECT * FROM resources')
+    .then((res) => {
+    // console.log('q: gARs r.r[3]:', res.rows[3]);
+      return res.rows;
+    })
+    .catch((err) => { console.error('error from DB', err) });
 };
 
 // //// getResource
 const getResource = (id) => {
-  let values = [id];
-  // console.log("q: gR: id ENTERED", id);
-  return pool.query("SELECT * FROM resources where id = $1", values)
-  .then(res => {
-    // console.log("q: gR r.r[0]:", res.rows[0]);
-    return res.rows;
-  })
+  const values = [id];
+  // console.log('q: gR: id ENTERED', id);
+  return pool.query('SELECT * FROM resources where id = $1', values)
+    .then((res) => {
+      // console.log('q: gR r.r[0]:', res.rows[0]);
+      return res.rows;
+    });
 };
 
 // //// getUser
 const getUser = (id) => {
-  let values = [id];
-  // console.log("q: gUs: id ENTERED", id);
-  return pool.query("SELECT * FROM users where id = $1", values)
-  .then(res => {
-    // console.log("q: gS r.rs:", res.rows);
-    return res.rows;
-  })
+  const values = [id];
+  // console.log('q: gUs: id ENTERED', id);
+  return pool.query('SELECT * FROM users where id = $1', values)
+    .then((res) => {
+      // console.log('q: gS r.rs:', res.rows);
+      return res.rows;
+    });
 };
 
 // //// getTopic
 const getTopic = (id) => {
-  let values = [id];
-  // console.log("q: gT: id ENTERED", id);
-  return pool.query("SELECT * FROM topics where id = $1", values)
-  .then(res => {
-    // console.log("q: gT: r.rs:", res.rows);
-    // return res.rows[0];
-    return res.rows;
-  })
+  const values = [id];
+  // console.log('q: gT: id ENTERED', id);
+  return pool.query('SELECT * FROM topics where id = $1', values)
+    .then((res) => {
+      // console.log('q: gT: r.rs:', res.rows);
+      // return res.rows[0];
+      return res.rows;
+    });
 };
 
 // //// updateResource
 const updateResource = (row) => {
-  // console.log("q: uR: ", row)
-  let values = [row.id, row.abbrev, row.contributor, row.description, row.leve, row.link, row.topic];
-  return pool.query("UPDATE resources SET (abbrev, contributor, description, level, link, topic) = ($2, $3, $4, $5, $6, $7) WHERE id = $1", values)
+  // console.log('q: uR: ', row)
+  const values = [row.id, row.abbrev, row.contributor, row.description, row.leve, row.link,
+    row.topic];
+  return pool.query('UPDATE resources SET (abbrev, contributor, description, level, link, topic) = ($2, $3, $4, $5, $6, $7) WHERE id = $1', values)
     .then(() => {
-      // console.log("q: uR: COMPLETE");
-      return true
-   })
-   .catch(err => {console.error("error from DB", err)})
+      // console.log('q: uR: COMPLETE');
+      return true;
+    })
+    .catch((err) => {console.error('error from DB', err)});
 };
 
-// console.log("q.js: LEAVING");
+// console.log('q.js: LEAVING');
 
 module.exports = {
   addResource,
@@ -129,6 +132,5 @@ module.exports = {
   getResource,
   getUser,
   getTopic,
-  updateResource
-}
-
+  updateResource,
+};
--- server/app.js
+++ server/app.js
@@ -1,132 +1,130 @@
 require('dotenv').config();
 const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-// const PORT = process.env.PORT || 26673; // Heroku default port
-
-// const bodyParser = require('body-parser');
-
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const pg = require('../db/query.js');
 
-// ////  get - tests that this file executes 
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// const bodyParser = require('body-parser'); QQQ NOT USED
+
+// get - tests that this file executes
 // app.get('/', (req, res) => {
-//   res.send("hello")
+//   res.send('hello')
 // });
 
-let pathname = path.join(__dirname, '../public/index.html');
-// console.log("app: pathname: ", pathname);
+const pathname = path.join(__dirname, '../public/index.html');
+// console.log('app: pathname: ', pathname);
 app.use(express.static(pathname));
-
-const cors = require('cors');
 app.use(cors());
-
 app.use(express.json());
 
-let entryTime = new Date();
+const entryTime = new Date().toLocaleTimeString();
 // console.log(new Date());
-console.log("a.js: ENTERING", new Date());
-console.log("PORT: ", PORT);
-console.log(__dirname);
+// console.log('a.js: ENTERING', entryTime);
+// console.log('PORT: ', PORT);
+// console.log(__dirname);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/../public/index.html'))
+  // res.sendFile(path.resolve(__dirname + '/../public/index.html'))
+  res.sendFile(path.join(__dirname, '/../public/index.html'));
 });
 
 app.get('/dist/bundle.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/../dist/bundle.js'))
+  // res.sendFile(path.resolve(__dirname + '/../dist/bundle.js'))
+  res.sendFile(path.join(__dirname, '/../dist/bundle.js'));
 });
 
-////// CRUD
+// CRUD
 
-// //// addResource
+// addResource
 app.post('/resources_db/resources/', (req, res) => {
   // let entryTime = new Date();
-  // console.log("a:: aR: ENTERED: req.url: ", req.url);
-  // console.log("a:: aR: ENTERED: req.body: ", req.body);
+  // console.log('a:: aR: ENTERED: req.url: ', req.url);
+  // console.log('a:: aR: ENTERED: req.body: ', req.body);
   pg.addResource(req.body)
-  .then((results) => {
-    // console.log("a:: aR: r.r.[0]: COMPLETED", results);
-    // console.log("duration to complete call: ", new Date() - entryTime, req.url);
-    res.sendStatus(201);
-  })
-  .catch(err => console.log(err));
+    .then((results) => {
+    // console.log('a:: aR: r.r.[0]: COMPLETED', results);
+    // console.log('duration to complete call: ', new Date() - entryTime, req.url);
+      res.sendStatus(201);
+    })
+    .catch((err) => console.log(err)); // eslint: unexpected console statement
 });
 
 
-// //// deleteResource
+// deleteResource
 app.delete('/resources_db/resources/:id', (req, res) => {
-  // console.log("a: dR: ENTERED: r.body[0]: ", req.body[0]);
+  // console.log('a: dR: ENTERED: r.body[0]: ', req.body[0]);
   pg.deleteResource(req.body[0])
     .then(() => {
-      // console.log("a: dR: COMPLETED");
+      // console.log('a: dR: COMPLETED');
       res.sendStatus(204); // null-ish
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
-// //// getAllResources
+// getAllResources
 app.get('/resources_db/resources', (req, res) => {
-  // console.log("a:: gARs: ENTERED");
+  // console.log('a:: gARs: ENTERED');
   pg.getAllResources()
-  .then((results) => {
-    let resultsJSON = JSON.stringify({data: results})
-    // console.log("a:: gARs: r.rJ[3]: COMPLETED", resultsJSON[3]);
-    res.send(resultsJSON);
-  })
-  .catch(err => console.log("app.js: gARs: err: ", err));
+    .then((results) => {
+      const resultsJSON = JSON.stringify({ data: results });
+      // console.log('a:: gARs: r.rJ[3]: COMPLETED', resultsJSON[3]);
+      res.send(resultsJSON);
+    })
+    .catch((err) => console.log('app.js: gARs: err: ', err));
 });
 
-// //// getResource
+// getResource
 app.get('/resources_db/resources/:id', (req, res) => {
-  console.log("a:: gR: ENTERED: req.id: ", req.params.id);
-  // console.log("a:: gOR: ENTERED");
+  // console.log('a:: gR: ENTERED: req.id: ', req.params.id);
+  // console.log('a:: gOR: ENTERED');
   pg.getResource(req.params.id)
-  .then((results) => {
-    // console.log("a:: gR: rs: COMPLETED", results);
-    res.send(results);
-  })
-  .catch(err => console.log(err));
+    .then((results) => {
+      // console.log('a:: gR: rs: COMPLETED', results);
+      res.send(results);
+    })
+    .catch((err) => console.log(err));
 });
 
-// //// getUser
+// getUser
 app.get('/resources_db/users/:id', (req, res) => {
-  // console.log("a:: gU: ENTERED");
+  // console.log('a:: gU: ENTERED');
   pg.getUser(req.params.id)
-  .then((results) => {
-    // console.log("a:: gU: rs: COMPLETED", results);
-    res.send(results);
-  })
-  .catch(err => console.log(err));
+    .then((results) => {
+      // console.log('a:: gU: rs: COMPLETED', results);
+      res.send(results);
+    })
+    .catch((err) => console.log(err));
 });
 
 // getTopic
 app.get('/resources_db/topics/:id', (req, res) => {
-  // console.log("a:: gT: ENTERED");
+  // console.log('a:: gT: ENTERED');
   pg.getTopic(req.params.id)
-  .then((results) => {
-    // console.log("a:: gT: rs: COMPLETED", results);
-    res.send(results);
-  })
-  .catch(err => console.log(err));
+    .then((results) => {
+      // console.log('a:: gT: rs: COMPLETED', results);
+      res.send(results);
+    })
+    .catch((err) => console.log(err));
 });
 
 
-// //// updateResource // put request handler, i.e. register a put with path, will call function
+// updateResource // put request handler, i.e. register a put with path, will call function
 app.put('/resources_db/resources/:id', (req, res) => {
-  // console.log("a:: uR: ENTERED");
-  // console.log("a:: uR: ENTERED: req.body: ", req.body);
+  // console.log('a:: uR: ENTERED');
+  // console.log('a:: uR: ENTERED: req.body: ', req.body);
   pg.updateResource(req.body)
-  .then((results) => {
-    // console.log("a:: uR: r.rs: COMPLETED", results);
-    res.sendStatus(201);
-  })
-  .catch(err => console.log(err));
+    .then((results) => {
+      // console.log('a:: uR: r.rs: COMPLETED', results);
+      res.sendStatus(201);
+    })
+    .catch((err) => console.log(err));
 });
 
-// console.log("a:: LEAVING");
+// console.log('a:: LEAVING');
 
 app.listen(PORT, () => {
-  console.log(`Web server running on: http://localhost:${PORT}`);
+  console.log(`Web server running on: http://localhost:${PORT}, at`, new Date().toLocaleTimeString());
 });
