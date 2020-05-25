3b7d18293fddb46c15c33e688c4182d22ad49911
nsbernstein52 <nsbernstein52@gmail.com>
2020-05-24 21:11:40 -0600

    Clean up names and formatting

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
-import AddResourceHandler from './comp/AddResourceHandler';
+import AddResource from './comp/AddResource';
 
 import './App.css';
 
@@ -146,85 +146,91 @@ class App extends React.Component {
       }
 
     return(
-      <Container-fluid className='layout' id='container-all'>
-      <div className='container-fluid'>
-        <Col className='layout #header'>
-          <Row className='layout'>
-            <Col className='layout' id='announcements'>
-              <b>HACKED RESOURCES</b>
-              {/* <br />  */}
-              <Row className='subheader'>
-                Find and contribute to Hack Reactor's Software Engineering Immersive (SEI) resources
-              </Row>
-            </Col>
-          </Row>.
-          <Row>
-          </Row>
-          <Row>
-              <div id='update-resource'>
-                <AddResourceHandler
-                  addResource={this.addResource}
-                />
-              </div>
-          </Row>
-          <br />
-          <Row className='instructions'>
-            <Col className='instr-caption' md={2}>
-              Sort by column:
-            </Col>
-            <Col className='instr-body' md={10}>
-              Click on column header
-            </Col>
-          </Row>
-          <Row>            
-            <Col className='instr-caption' md={2}>
-              Edit cell: 
-            </Col>
-            <Col className='instr-body' md={10}>
-              Double-click in cell (for &quot;link&quot;, click to right side of link), change text, press ENTER (or ESC to cancel)<br />
-            </Col>
-          </Row>
-          <Row>
-            <Col className='instr-caption' md={2}>
-              Delete row: 
-            </Col>
-            <Col className='instr-body' md={10}>
-              Click on radio button, click on DELETE<br />
-            </Col>
-          </Row>
-          <Row>            
-            <Col className='instr-caption' md={2}>
-              Filter by column: 
-            </Col>
-            <Col className='instr-body' md={10}>
-              Enter text (not case sensitive) in box at bottom of column<br /> &nbsp;
-            </Col>
-          </Row>
-          <Row className='bst'>
-            <BootstrapTable 
-              cellEdit = { cellEdit }
-              columnFilter 
-              condensed 
-              data = { resourcesArr } 
-              deleteRow
-              hover 
-              options = { rbtOptions }
-              selectRow = { selectRow }
-              striped
-              version = '4' 
-            > 
-              <TableHeaderColumn isKey dataField='id' dataSort width='6%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; ID</TableHeaderColumn>
-              <TableHeaderColumn dataField='topic' dataSort width='15%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; Topic</TableHeaderColumn>
-              <TableHeaderColumn dataField='abbrev' dataSort width='8%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; Abbrev</TableHeaderColumn>
-              <TableHeaderColumn dataField='link' dataSort dataFormat={ this.topicFormatter} width='6%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; Link</TableHeaderColumn>
-              <TableHeaderColumn dataField='level' dataSort width='8%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; Level</TableHeaderColumn>
-              <TableHeaderColumn dataField='contributor' dataSort width='10%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; Contributor</TableHeaderColumn>
-              <TableHeaderColumn dataField='description' dataSort width='46%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; Description</TableHeaderColumn>
-            </BootstrapTable>
-          </Row>
+      <Container-fluid>
+        <div className='container-fluid'>
+          <Col>
+            <Row className='page-header'>
+              <Col>
+                <b>HACKED RESOURCES</b>
+                <Row className='subheader'>
+                  Find and contribute to Hack Reactor's Software Engineering Immersive (SEI) resources
+                </Row>
+              </Col>
+            </Row>
 
-        </Col>
-      </div>
+            <Row>
+              <Col className='instr-caption' md={2}>
+                Sort by column:
+              </Col>
+              <Col className='instr-body' md={10}>
+                Click on column header
+              </Col>
+            </Row>
+            <Row>            
+              <Col className='instr-caption' md={2}>
+                Edit cell: 
+              </Col>
+              <Col className='instr-body' md={10}>
+                Double-click in cell (for &quot;link&quot;, click to right side of link), change text, press ENTER (or ESC to cancel)
+              </Col>
+            </Row>
+            <Row>
+              <Col className='instr-caption' md={2}>
+                Delete row: 
+              </Col>
+              <Col className='instr-body' md={10}>
+                Click on radio button, click on DELETE
+              </Col>
+            </Row>
+            <Row>            
+              <Col className='instr-caption' md={2}>
+                Filter by column: 
+              </Col>
+              <Col className='instr-body' md={10}>
+                Enter text (not case sensitive) in box at bottom of column
+              </Col>
+            </Row>
+            <Row>            
+              <Col className='instr-caption' md={2}>
+                Add a resource: 
+              </Col>
+              <Col className='instr-body' md={10}>
+                Use <a href='#add-resource'>Add Resource</a> form at the bottom of the page
+              </Col>
+            </Row>
+
+            <Row className='bootstrap-table-row'>
+              <BootstrapTable 
+                cellEdit = { cellEdit }
+                columnFilter 
+                condensed 
+                data = { resourcesArr } 
+                deleteRow
+                hover 
+                options = { rbtOptions }
+                selectRow = { selectRow }
+                striped
+                version = '4' 
+              > 
+                <TableHeaderColumn isKey dataField='id' dataSort width='6%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; ID</TableHeaderColumn>
+                <TableHeaderColumn dataField='topic' dataSort width='15%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; Topic</TableHeaderColumn>
+                <TableHeaderColumn dataField='abbrev' dataSort width='8%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; Abbrev</TableHeaderColumn>
+                <TableHeaderColumn dataField='link' dataSort dataFormat={ this.topicFormatter} width='6%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; Link</TableHeaderColumn>
+                <TableHeaderColumn dataField='level' dataSort width='8%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; Level</TableHeaderColumn>
+                <TableHeaderColumn dataField='contributor' dataSort width='10%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; Contributor</TableHeaderColumn>
+                <TableHeaderColumn dataField='description' dataSort width='46%' tdStyle={{ whiteSpace: 'normal' }}>&#x2195; Description</TableHeaderColumn>
+              </BootstrapTable>
+            </Row>
+          </Col>
+        </div>
+        <div>
+          <a name='add-resource'></a>
+            <Row className='add-resource-row'>
+              <AddResource className='add-resource-call'
+                addResource={this.addResource}
+              />
+            </Row>
+        </div>
       </Container-fluid>
 
     )
