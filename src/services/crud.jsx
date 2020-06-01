45e35eaea809dd95e6e7b4cebecadc9672a6e037
nsbernstein52 <nsbernstein52@gmail.com>
2020-05-31 19:16:35 -0600

    Work on bugs

--- src/services/crud.jsx
+++ src/services/crud.jsx
@@ -1,34 +1,20 @@
 const path = require('path');
 
+const pathResources = '/resources_db/resources/';
+
 // crud functions
 
-const addResource = (event, abbrev, contributor, description, level, link, topic, callback) => {
-  event.preventDefault();
-  const resourceObj = {
-    abbrev, contributor, description, level, link, topic,
-  };
-
-  // naming QQQ
-  // abbrev: abbrev,
-  // contributor: contributor,
-  // description: description,
-  // level: level,
-  // link: link,
-  // topic: topic
-
-  fetch('/resources_db/resources/', {
+const addResource = (resourceObj, callback) => {
+  fetch(`${pathResources}`, {
     method: 'POST',
     headers: {
-      Accept: 'application/json',
+      Accept: 'application/json', // QQQ
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(resourceObj),
   })
-    .then(response => { // why eslnt catch?
-      this.setState({
-        resourcesChanged: true,
-      });
-    })
+    .then((response) => response.json())
+    .then((data) => callback(data))
     .catch((error) => console.error('crud: aR: catch: ', error)); // eslint-disable-line
 };
 
@@ -51,53 +37,46 @@ const addResource = (event, abbrev, contributor, description, level, link, topic
 //   .catch(error => console.error('crud: dR: catch: ', error));
 // };
 
-const deleteResource = (rowKeys) => {
-  this.setState({
-    resourcesArr: this.state.resourcesArr.filter((elem) => {
-      return elem.id !== rowKeys[0];
-    }), // how???
-  }, () => {
-    event.preventDefault();
-    const pathResoures = '/resources_db/resources/';
-    const pathRowKeys0 = path.join(pathResoures, rowKeys[0]);
-    fetch(pathRowKeys0, {
-    // fetch('/resources_db/resources/' + rowKeys[0], {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(response => {
-        this.setState({
-          resourcesChanged: true,
-        });
-      })
-      .catch((error) => console.error('crud: dR: catch: ', error)); // eslint-disable-line
-  });
-};
-
-const updateResource = (row, cellName, cellValue) => {
-  const pathResoures = '/resources_db/resources/';
-  const pathRowId = path.join(pathResoures, row.id);
-  fetch(pathRowId, {
-  // fetch('/resources_db/resources/' + row.id, {
-    method: 'PUT',
+const deleteResource = (rowKeys, callback) => {
+  const pathRowKeys0 = path.join(pathResources, rowKeys[0]);
+  fetch(`${pathRowKeys0}`, {
+    method: 'DELETE',
     headers: {
-      Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(row),
   })
-    .then(response => {
-      this.setState({
-        resourcesChanged: true,
-      });
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch((error) => console.error('crud: dR: catch: ', error)); // eslint-disable-line
+};
+
+const getAllResources = (callback) => {
+  fetch(`${pathResources}`)
+    .then((resources) => {
+      return resources.json();
     })
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch((error) => console.error('crud: gAR: catch: ', error)); // eslint-disable-line
+};
+
+const updateResource = (row, cellName, cellValue, callback) => {
+  const pathRowId = path.join(pathResources, row.id);
+  fetch(`${pathRowId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json', // QQQ
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => callback(data))
     .catch((error) => console.error('crud: uR: catch: ', error)); // eslint-disable-line
 };
 
 module.exports = {
   addResource,
   deleteResource,
+  getAllResources,
   updateResource,
 };
