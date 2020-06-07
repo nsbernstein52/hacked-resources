10f48ff43d7e7ebbf4ea61785d654a8580e83aca
nsbernstein52 <nsbernstein52@gmail.com>
2020-06-07 16:40:52 -0600

    Organize comments

--- db/schema.sql
+++ db/schema.sql
@@ -15,10 +15,10 @@ CREATE TABLE resources (
 
 /*
 COPY resources (abbrev, contributor, description, level, link, topic) FROM '/Users/nsb52/mvp/hr_data.20200514.a.csv' DELIMITER ',' CSV HEADER;
-Heroku:
-\COPY resources (abbrev, contributor, description, level, link, topic) FROM '/Users/nsb52/mvp/hr_data.20200514.a.csv' DELIMITER ',' CSV HEADER;
 DELETE FROM resources * where id > 159;
 ALTER SEQUENCE resources_id_seq RESTART 160;
 TRUNCATE resources;
 ALTER SEQUENCE resources_id_seq RESTART 1;
+Heroku:
+\COPY resources (abbrev, contributor, description, level, link, topic) FROM '/Users/nsb52/mvp/hr_data.20200514.a.csv' DELIMITER ',' CSV HEADER;
 */
